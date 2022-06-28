230e55266323b8f7399d11c6bc0bc642ffe477cc
hj-language <idgafff@kumoh.ac.kr>
2022-06-28 15:17:30 +0900

    22/06/28 이메일 인증용 id, pw 변수로 사용 및 transporter 모듈명 수정

--- router/sign.js
+++ router/sign.js
@@ -4,6 +4,7 @@ const User = require("../schemas/user");
 const nodemailer = require("nodemailer");
 const ejs = require("ejs");
 const path = require("path");
+const verifyEmail = require("../secret.js").verifyEmail;
 require("dotenv").config();
 
 var appDir = path.dirname(require.main.filename);
@@ -33,10 +34,10 @@ router.post("/", async (req, res) => {
         if (dupName) return res.status(403).send("사용중인 닉네임입니다.");
         if (dupEmail)
             return res.status(403).send("이미 가입된 회원정보가 존재합니다.");
-
+        
         let email = req.body.webmail;
         const emailValid = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@kumoh\.ac.kr$/;
-
+        
         if (!emailValid.test(email)) {
             return res.json({
                 message: "금오공과대학교 웹메일을 통한 가입만 가능합니다 :)",
@@ -48,22 +49,22 @@ router.post("/", async (req, res) => {
          *  1. 인증번호를 통한 이메일 인증?
          *  2. 인증사이트 링크 클릭을 통해 인증?
          */
-
-         let transporter = mail.createTransport({
+        
+        let transporter = nodemailer.createTransport({
             service: 'naver',
             auth: {
-                user: '사용할 호스트 메일 주소',
-                pass: '호스트 메일 계정 비밀번호'
+                user: verifyEmail.id,
+                pass: verifyEmail.pw
             }
         });
         
         let mailOptions = {
-            from: '호스트 메일 ID',
+            from: verifyEmail.id,
             to: email, // 대상 메일 주소 req.body.email
             subject: '[CE-CS]Authorization Test Mail', // 메일 제목
             text: 'testnum' // 메일 내용
         };
-    
+        
         transporter.sendMail(mailOptions, function(err, info){
             if (err) {
                 console.log(err);
@@ -72,6 +73,7 @@ router.post("/", async (req, res) => {
                 console.log('Email sent: ' + info.response);
             }
         });
+
         // 응답 구현 해야함
         //res.redirect("/");
 
