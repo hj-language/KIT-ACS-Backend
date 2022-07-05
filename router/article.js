d0fcad682eea52489bd3386a9d5770581aae8441
Junsu <pksjmh5295@gmail.com>
2022-07-05 18:52:36 +0900

    22/07/05 게시물 article 기본 CRUD

--- router/article.js
+++ router/article.js
@@ -12,7 +12,7 @@ router.post("/", (req, res) => {
         tag: req.body.tag,
         content: req.body.content,
         views: req.body.views,
-        commentList: req.body.commentList
+        commentList: req.body.commentList,
     });
     obj.save((err) => console.log("error: ", err));
     Article.find((err, user) => {
@@ -20,6 +20,89 @@ router.post("/", (req, res) => {
         else console.log(user);
     });
     res.status(200).end();
+});
+
+// Status Code
+// 400 Bad Request
+// 401 Unauthorized
+// 403 Forbidden
+// 404 Not Found
+// 500 Internal Server Error
+
+// 전체 게시물 조회
+router.get("/", async (req, res) => {
+    try {
+        const results = await Article.find({ ...Article });
+        // console.log(results);
+        return res.json(results).status(200);
+
+        // res.render({ posts: results })
+        // res.status(200).end();
+    } catch (e) {
+        return res.status(500).send();
+    }
+});
+
+// 특정(_id) 게시물 조회
+router.get("/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const results = await Article.findOne({ _id, ...Article });
+        if (!results) {
+            return res.status(404).send();
+        }
+        // console.log(results);
+        return res.json(results).status(200);
+        // res.render("post", { posts: results });
+    } catch (e) {
+        return res.status(500).send();
+    }
+});
+
+// 게시물 update (_id 기반)
+router.patch("/:id", async (req, res) => {
+    const article = Object.keys(req.body);
+    const allowedUpdates = ["title", "content"]; // 변경 가능한 것 (제목, 내용)
+    // console.log(article);
+
+    // patch 보낼 때 변경 가능한 거만 보내기
+    // {
+    // 	"title": "test WR3",
+    // 	"content": "UPDATE"
+    // }
+    const updateValid = article.every((update) =>
+        allowedUpdates.includes(update)
+    );
+
+    if (!updateValid) {
+        return res.status(400).send({ error: "Cannot Update" });
+    }
+
+    try {
+        const edit = await Article.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!edit) {
+            return res.status(404).send();
+        }
+        res.send(edit);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+// 게시물 삭제 (_id 기반)
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Article.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).send();
+        }
+        res.send(post);
+    } catch (e) {
+        res.status(500).send();
+    }
 });
 
 /*
