b736a0d77223bad2c297668514b0f23263c078e0
Junsu <pksjmh5295@gmail.com>
2022-07-06 00:38:13 +0900

    22/07/06 email 인증

--- router/sign.js
+++ router/sign.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 const ejs = require("ejs");
 const path = require("path");
 const verifyEmail = require("../secret.js").verifyEmail;
+const crypto = require("crypto");
 require("dotenv").config();
 
 var appDir = path.dirname(require.main.filename);
@@ -25,7 +26,10 @@ router.post("/del", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    // console.log(req.body.password);
+    User.find((err, user) => {
+        if (err) console.log(err);
+        else console.log(user);
+    });
     try {
         const dupId = await User.findOne({ id: req.body.id });
         const dupName = await User.findOne({ name: req.body.name });
@@ -34,10 +38,10 @@ router.post("/", async (req, res) => {
         if (dupName) return res.status(403).send("사용중인 닉네임입니다.");
         if (dupEmail)
             return res.status(403).send("이미 가입된 회원정보가 존재합니다.");
-        
+
         let email = req.body.webmail;
         const emailValid = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@kumoh\.ac.kr$/;
-        
+
         if (!emailValid.test(email)) {
             return res.json({
                 message: "금오공과대학교 웹메일을 통한 가입만 가능합니다 :)",
@@ -46,36 +50,42 @@ router.post("/", async (req, res) => {
 
         /**
          * 인증방법
-         *  1. 인증번호를 통한 이메일 인증?
-         *  2. 인증사이트 링크 클릭을 통해 인증?
+         *
+         *  인증사이트 링크 클릭을 통해 인증
          */
-        
+
+        const toHash = `${req.body.id}${req.body.name}${req.body.webmail}`;
+        const code = crypto.createHash("sha256").update(toHash).digest("hex");
+
         let transporter = nodemailer.createTransport({
-            service: 'naver',
+            service: "naver",
             auth: {
                 user: verifyEmail.id,
-                pass: verifyEmail.pw
-            }
+                pass: verifyEmail.pw,
+            },
         });
-        
+
         let mailOptions = {
             from: verifyEmail.id,
             to: email, // 대상 메일 주소 req.body.email
-            subject: '[CE-CS]Authorization Test Mail', // 메일 제목
-            text: 'testnum' // 메일 내용
+            subject: "[ACS]Authorization Test Mail", // 메일 제목
+            html: `<h1>이메일 인증</h1>
+            <div>
+                아래 버튼을 눌러 인증을 완료해주세요.
+            </div>
+            <div>
+                <a href='http://localhost:3000/sign/confirmEmail?code=${code}&email=${email}'>이메일 인증하기</a>
+            </div>`,
         };
-        
-        transporter.sendMail(mailOptions, function(err, info){
+
+        transporter.sendMail(mailOptions, function (err, info) {
             if (err) {
                 console.log(err);
+            } else {
+                console.log("Email sent: " + info.response);
             }
-            else {
-                console.log('Email sent: ' + info.response);
-            }
+            transporter.close();
         });
-
-        // 응답 구현 해야함
-        //res.redirect("/");
 
         let user = new User({
             id: req.body.id,
@@ -90,6 +100,21 @@ router.post("/", async (req, res) => {
     } catch (err) {}
 });
 
-router.get("/confirmEmail", (req, res) => {});
+router.get("/confirmEmail", (req, res) => {
+    // console.log(req.query);
+    userConfirm = User.findOne({ webmail: req.query.email }, (err, user) => {
+        const _id = user._id;
+
+        const toHash = `${user.id}${user.name}${user.webmail}`;
+        const verify = crypto.createHash("sha256").update(toHash).digest("hex");
+
+        if (verify === req.query.code) {
+            User.findByIdAndUpdate(_id, { $set: { verify: true } }).exec();
+            return res.status(200).redirect(`/`);
+        } else {
+            console.log("fuck!");
+        }
+    });
+});
 
 module.exports = router;
