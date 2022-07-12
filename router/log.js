fca09134bd022a87ee649e32f4a3c5ed9d45e8f4
Junsu <pksjmh5295@gmail.com>
2022-07-12 20:59:31 +0900

    22/07/12 mypage, mypage 비밀번호 변경

--- router/log.js
+++ router/log.js
@@ -3,31 +3,35 @@ const router = express.Router();
 const User = require("../schemas/user");
 
 router.post("/in", (req, res) => {
-    if (req.session.authorization) { // 이미 세션이 존재한다면
-        req.session.destroy(() => { // 세션 삭제 후 다시 시도하게 함
-            res.status(400)
-                .json({ message: 'Try again'});
-        })
+    if (req.session.authorization) {
+        // 이미 세션이 존재한다면
+        req.session.destroy(() => {
+            // 세션 삭제 후 다시 시도하게 함
+            res.status(400).json({ message: "Try again" });
+        });
         return;
     }
 
     User.findOne({ id: req.body.id }, (err, user) => {
         console.log(user);
         if (!user || !user.verify) {
-            return res.json({
-                message: "Invalid ID",
-            });
+            return res
+                .json({
+                    message: "Invalid ID",
+                })
+                .status(400);
         }
         user.comparePassword(req.body.password, (_, isMatch) => {
             if (!isMatch) {
-                return res.json({
-                    message: "Wrong ID or Password",
-                });
+                return res
+                    .json({
+                        message: "Wrong ID or Password",
+                    })
+                    .status(400);
             }
             req.session.authorization = user.id;
-            req.session.cookie.expires = new Date(Date.now() + 10*30000), // 30초
-            res.status(200)
-                .json({ message: `Welcome! ${user.name}` })
+            (req.session.cookie.expires = new Date(Date.now() + 10 * 30000)), // 30초
+                res.status(200).json({ message: `Welcome! ${user.name}` });
         });
     });
 });
@@ -36,13 +40,10 @@ router.delete("/out", (req, res) => {
     console.log(req.session);
     if (req.session.authorization) {
         req.session.destroy(() => {
-            res.status(200)
-            .json({ message: 'Goodbye!'})
-        })
-    }
-    else {
-        res.status(400)
-        .json({message: 'There is no session'})
+            res.status(200).json({ message: "Goodbye!" });
+        });
+    } else {
+        res.status(400).json({ message: "There is no session" });
     }
 });
 
