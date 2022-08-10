e0499bba962e2215672fef41a19fa13e3c8bb544
hj-language <idgafff@kumoh.ac.kr>
2022-08-10 22:12:39 +0900

    22/08/10 필요없는 부분 제거

--- router/article.js
+++ router/article.js
@@ -11,13 +11,6 @@ const fs = require("fs")
 const { verifyUser, checkPermission } = require("./middlewares/authorization")
 const paging = require("./js/pagination")
 
-// Status Code
-// 400 Bad Request
-// 401 Unauthorized
-// 403 Forbidden
-// 404 Not Found
-// 500 Internal Server Error
-
 const isUserClassOne = async (id) => {
     const user = await User.findOne({ id: id })
     return user.class === 1
