e3d02d0fa2d7e55249ca15f5bd426d592f7d8b08
KimWonRyeol <69651381+KimWonRyeol@users.noreply.github.com>
2022-08-13 01:33:43 +0900

    22/08/13 mypage Paging

--- router/mypage.js
+++ router/mypage.js
@@ -5,6 +5,7 @@ const User = require("../schemas/user")
 const bcrypt = require("bcrypt")
 const verifyUser = require("./middlewares/authorization").verifyUser
 const saltFactor = require("../secret.js").saltFactor
+const paging = require("./js/pagination")
 
 // Status Code
 // 400 Bad Request
@@ -19,26 +20,33 @@ router.get("/", verifyUser, async (req, res) => {
     if (!user) {
         return res.status(403).send({ message: "Forbidden" })
     }
-
     try {
-        Article.find({ author: user.id }, (e, articles) => {
-            if (e) {
-                console.log("error: ", e)
-                res.status(500).send({ message: "Server Error" })
-            } else {
-                const userMypage = {
-                    id: user.id,
-                    name: user.name,
-                    email: user.webmail,
-                    articles: articles,
-                }
-                res.status(200).json(userMypage)
-            }
-        })
+        const { page, limit } = req.query
+
+        const totalArticle = await Article.countDocuments({ author: user.id })
+
+        let { startPage, endPage, hidePost, postLimit, totalPages, pageNum } =
+            paging(page, totalArticle, limit)
+
+        const articles_ = await Article.find({ author: user.id })
+            .sort({ date: -1 })
+            .skip(hidePost)
+            .limit(postLimit)
+
+
+        const userMypage = {
+            id: user.id,
+            name: user.name,
+            email: user.webmail,
+            articles: articles_,
+        }
+        res.status(200).json(userMypage)
     } catch (e) {
         console.log("error: ", e)
         res.status(500).send({ message: "Server Error" })
     }
+
+
 })
 
 // 비밀번호 수정
