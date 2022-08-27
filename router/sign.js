423f79bc099455411b4ae9e364e70639ba2d015b
Kimwonryeol <wonryeol3943@gmail.com>
2022-08-27 17:49:28 +0900

    22/08/27 비밀번호 찾기 및 변경(아직 response 설정 못 함, 기능은 됨)

--- router/sign.js
+++ router/sign.js
@@ -6,6 +6,7 @@ const Comment = require("../schemas/comment")
 const nodemailer = require("nodemailer")
 const verifyEmail = require("../secret.js").verifyEmail
 const crypto = require("crypto")
+const { findOneAndUpdate, findOneAndDelete, findOne, findById, findByIdAndUpdate } = require("../schemas/article")
 require("dotenv").config()
 const verifyUser = require("./middlewares/authorization").verifyUser
 
@@ -21,12 +22,95 @@ router.get("/findID", async (req, res) => {
             console.log("error: ", e)
             return res.status(500).send({ message: "Server Error" })
         }
-        
+
         return res.json(user.id).status(200)
     })
-
 })
 
+router.get("/userdelete", (req,res) => {
+    User.deleteOne({name: "김원렬"})
+})
+
+router.get("/findPassword", async (req, res) => {
+    try
+    {
+    User.findOne({ id: req.body.id, name: req.body.name} ,async (e, user) => {
+        if (!user) {
+            return res.status(404).send({ message: "Not exist" })
+        }
+        if (e) {
+            console.log("error: ", e)
+            return res.status(500).send({ message: "Server Error" })
+        }
+        
+        const email = user.webmail
+        const toHash = `${user.id}${user.name}${user.webmail}`
+        const code = crypto.createHash("sha256").update(toHash).digest("hex")
+
+        let transporter = nodemailer.createTransport({
+            service: "naver",
+            auth: {
+                user: verifyEmail.id,
+                pass: verifyEmail.pw,
+            },
+        })
+
+        let mailOptions = {
+            from: verifyEmail.id,
+            to: email, // 대상 메일 주소 req.body.email
+            subject: "[ACS]비밀번호 초기화 메일 인증", // 메일 제목
+            html: `
+            <div style="border: black 2px solid; border-radius: 1rem; margin-right: 1rem; padding: 1rem; text-align: center; max-width: 500px;">
+                <div style="font-size: 2rem">비밀번호 변경</div></br>
+                <div style="font-size: 1.2rem">
+                    아래 버튼을 눌러서 비밀번호 변경을 진행해주세요.
+                </div>
+                </br>
+                <div style="margin: 0.2rem;">
+                    <a 
+                        style="background-color: lightblue; border-radius: 10px; padding: 0.5rem;"
+                        href='http://localhost:3001/sign/changePassword?code=${code}&email=${email}'>비밀번호 변경하기</a>
+                </div>
+            </div>
+            `,
+        }
+
+        transporter.sendMail(mailOptions, function (e, info) {
+            if (e) {
+                console.log("error: ", e)
+                res.status(500).send({ message: "Server Error" })
+            } else console.log("Email sent: " + info.response)
+            transporter.close()
+        })
+        res.status(201).send("메일 보내기 성공")
+    })
+    } catch (e) {
+        console.log("error: ", e)
+        res.status(500).send({ message: "Server Error" })
+    }
+})
+
+//비번 바꾸는 거니까  post로 했으면 좋겠는데 url 들어가면 기본이 get이니까 get으로 일단 함
+
+
+router.get("/changePassword", (req, res) => {
+    
+    //이거 해야 대나?
+    let toHash, code
+    User.findOne({webmail: req.query.email} ,async (e, user) => {
+        user.password =  req.body.newPassword
+        console.log(user.password)
+        await user.save()
+        console.log(user)
+    })
+})
+
+
+//    if (code === req.query.code) {
+//        return res.status(200).send({ message: "Success" })
+//   } else {
+//        return res.status(403).send({ message: "Invalid code" })
+//   }
 
 
 router.post("/up", async (req, res) => {
@@ -72,7 +156,7 @@ router.post("/up", async (req, res) => {
         let mailOptions = {
             from: verifyEmail.id,
             to: email, // 대상 메일 주소 req.body.email
-            subject: "[ACS]Authorization Test Mail", // 메일 제목
+            subject: "[ACS]회원가입 메일 인증", // 메일 제목
             html: `
             <div style="border: black 2px solid; border-radius: 1rem; margin-right: 1rem; padding: 1rem; text-align: center; max-width: 500px;">
                 <div style="font-size: 2rem">이메일 인증</div></br>
@@ -83,7 +167,7 @@ router.post("/up", async (req, res) => {
                 <div style="margin: 0.2rem;">
                     <a 
                         style="background-color: lightblue; border-radius: 10px; padding: 0.5rem;"
-                        href='http://localhost:3000/sign/confirmEmail?code=${code}&email=${email}'>이메일 인증하기</a>
+                        href='http://localhost:3001/sign/confirmEmail?code=${code}&email=${email}'>이메일 인증하기</a>
                 </div>
             </div>
             `,
